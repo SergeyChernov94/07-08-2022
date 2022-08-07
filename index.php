280da5f6655bb1e0162db342835cd008dfe5f3ef
SergeyChernov94 <107950509+SergeyChernov94@users.noreply.github.com>
2022-08-07 17:13:28 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+require_once "form.html";
+
+exit;
+
+/* $a = rand(0, 100);
+$b = rand(0, 100);
+echo "$a + $b" . $a + $b;
+
+
+$randomString - '';
+for ($i = 0; $i < $length; $i++) {
+    $randomString .=$characters[rand(0, $charactersLenght - 1)]}
+return $randomString
+}
+$str_one = generateRandomString();
+$str_two = generateRandomString(9);
+
+echo "$str_one + $str_two = " . $str_one + $str_two; 
+
+echo "<br><br><br><br>";
+
+$dog = "Шарик";
+$cat = "Барсик";
+$fish = "Немо";
+
+$type_animals = ['dog', 'cat', 'fish', 'buldog'];
+
+$i = 0;
+while ($i < 5) {
+    $type_animal = $type_animals[rand(0, count($type_animals) - 1)];
+
+    if (isset($$type_animal)) {
+        echo $$type_animal;
+    } else {
+        echo "Error!!!";
+    }
+    echo "<br>";
+    $i++;
+}
+
+
